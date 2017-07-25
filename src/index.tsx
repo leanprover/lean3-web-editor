fad0f0343dd2f3ae1313e1ed39951a50a4750707
Gabriel Ebner <gebner@gebner.org>
2017-07-25 17:33:13 +0100

    Goal view.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 /// <reference types="monaco-editor" />
 import { InfoRecord, LeanJsOpts, Message, Severity } from 'lean-client-js-browser';
 import * as React from 'react';
-import { render } from 'react-dom';
+import { findDOMNode, render } from 'react-dom';
 import MonacoEditor from 'react-monaco-editor';
 import * as SplitPane from 'react-split-pane';
 import {registerLeanLanguage, server} from './langservice';
+
+const codeBlockStyle = {
+  display: 'block',
+  fontFamily: 'monospace',
+  whiteSpace: 'pre-wrap',
+  marginTop: '1em',
+};
 
 interface MessageWidgetProps {
   msg: Message;
@@ -21,29 +28,31 @@ function MessageWidget({msg}: MessageWidgetProps) {
       <div style={{ borderBottom: '1px solid', fontFamily: 'sans-serif',
           fontWeight: 'bold', color: colorOfSeverity[msg.severity] }}>
         {msg.pos_line}:{msg.pos_col}: {msg.severity}: {msg.caption}</div>
-      <pre>{msg.text}</pre>
+      <div style={codeBlockStyle}>{msg.text}</div>
     </div>
   );
 }
 
 interface GoalWidgetProps {
   goal: InfoRecord;
+  position: monaco.Position;
 }
-function GoalWidget({goal}: GoalWidgetProps) {
+function GoalWidget({goal, position}: GoalWidgetProps) {
   return (
     <div style={{paddingBottom: '1em'}}>
       <div style={{borderBottom: '1px solid', fontWeight: 'bold', fontFamily: 'sans-serif'}}>
-        goal at {goal.source.line}:{goal.source.column}</div>
-      <pre>{goal.state}</pre>
+        goal at {position.lineNumber}:{position.column - 1}</div>
+      <div style={codeBlockStyle}>{goal.state}</div>
     </div>
   );
 }
 
 interface InfoViewProps {
   file: string;
+  cursor?: monaco.Position;
 }
 interface InfoViewState {
-  goal?: InfoRecord;
+  goal?: GoalWidgetProps;
   messages: Message[];
 }
 class InfoView extends React.Component<InfoViewProps, InfoViewState> {
@@ -58,7 +67,6 @@ class InfoView extends React.Component<InfoViewProps, InfoViewState> {
     this.subscriptions.push(
       server.allMessages.on((allMsgs) => {
         this.setState({
-          goal: this.state.goal,
           messages: allMsgs.msgs.filter((v) => v.file_name === this.props.file),
         });
         // this.forceUpdate();
@@ -74,13 +82,30 @@ class InfoView extends React.Component<InfoViewProps, InfoViewState> {
   }
 
   render() {
+    const goal = this.state.goal && (<div key={'goal'}>{GoalWidget(this.state.goal)}</div>);
     const msgs = this.state.messages.map((msg, i) =>
       (<div key={i}>{MessageWidget({msg})}</div>));
     return (
       <div>
+        {goal}
         {msgs}
       </div>
     );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.refreshGoal();
+  }
+
+  refreshGoal() {
+    if (!this.props.cursor) {
+      return;
+    }
+
+    const position = this.props.cursor;
+    server.info(this.props.file, position.lineNumber, position.column - 1).then((res) => {
+      this.setState({goal: res.record && res.record.state && {goal: res.record, position}});
+    });
   }
 }
 
@@ -89,14 +114,16 @@ interface LeanEditorProps {
   persistent: boolean;
 }
 interface LeanEditorState {
-  code: string;
+  cursor?: monaco.Position;
 }
 class LeanEditor extends React.Component<LeanEditorProps, LeanEditorState> {
-  editor: monaco.editor.ICodeEditor;
+  model: monaco.editor.IModel;
+  editor: monaco.editor.IStandaloneCodeEditor;
 
   constructor(props) {
     super(props);
-    this.state = { code: this.getStoredCode() };
+    this.state = {};
+    this.model = monaco.editor.createModel(this.getStoredCode(), 'lean', monaco.Uri.file(this.props.file));
   }
 
   getStoredCode() {
@@ -114,50 +141,42 @@ class LeanEditor extends React.Component<LeanEditorProps, LeanEditorState> {
     }
   }
 
-  editorDidMount(editor: monaco.editor.ICodeEditor) {
-    this.editor = editor;
-  }
-
-  onChange(newValue, e: monaco.editor.IModel) {
-    this.saveCode();
-  }
-
-  changeEditorValue() {
-    if (this.editor) {
-      // this.editor.setValue('// code changed! \n');
-    }
-  }
-
-  changeBySetState() {
-    // this.setState({code: '// code changed by setState! \n'});
-  }
-
-  render() {
-    const code = this.state.code;
+  componentDidMount() {
+    const node = findDOMNode<HTMLElement>(this.refs.monaco);
     const options: monaco.editor.IEditorConstructionOptions = {
       selectOnLineNumbers: true,
       roundedSelection: false,
       readOnly: false,
       theme: 'vs',
       cursorStyle: 'line',
-      automaticLayout: false,
+      // automaticLayout: true,
       cursorBlinking: 'solid',
-      model: monaco.editor.createModel(code, 'lean', monaco.Uri.file(this.props.file)),
+      model: this.model,
     };
+    this.editor = monaco.editor.create(node, options);
+    this.editor.onDidChangeModel((e) => this.saveCode());
+    this.editor.onDidChangeCursorPosition((e) => this.setState({cursor: e.position}));
+    window.addEventListener('resize', this.updateDimensions.bind(this));
+  }
+  componentWillUnmount() {
+    this.editor.dispose();
+    this.editor = undefined;
+    window.removeEventListener('resize', this.updateDimensions.bind(this));
+  }
+
+  updateDimensions() {
+    this.editor.layout();
+  }
+
+  render() {
     return (
-      <div style={{display: 'grid', gridGap: '2em'}}>
-        <div style={{gridColumn: 1}}>
-          <MonacoEditor
-              height='800'
-              language='lean'
-              value={code}
-              options={options}
-              editorDidMount={(e) => this.editorDidMount(e)}
-              onChange={(v, e) => this.onChange(v, e)}
-          />
+      <div style={{display: 'grid', gridGap: '2em', height: '95vh', width: '95vw', gridTemplateColumns: '50% 50%',
+          gridTemplateRows: '100%'}}>
+        <div style={{gridColumn: 1, overflow: 'hidden'}}>
+          <div ref='monaco' style={{height: 'calc(100% - 35px)', width: '100%', overflow: 'hidden'}}/>
         </div>
         <div style={{gridColumn: 2}}>
-          <InfoView file={this.props.file}/>
+          <InfoView file={this.props.file} cursor={this.state.cursor}/>
         </div>
       </div>
     );

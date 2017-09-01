25cf36e4be31531181b10a446f0b924d5ccbf200
Gabriel Ebner <gebner@gebner.org>
2017-09-01 16:27:26 +0200

    fetch_lean_js: use current url

--- fetch_lean_js.sh
+++ fetch_lean_js.sh
@@ -3,7 +3,7 @@ set -ex
 
 mkdir -p dist
 
-base_url=https://gebner.github.io/lean-web-editor
+base_url=https://leanprover.github.io/live/latest/
 
 for i in lean_js_js.js lean_js_wasm.js lean_js_wasm.wasm library.zip; do
     curl $base_url/$i -o dist/$i

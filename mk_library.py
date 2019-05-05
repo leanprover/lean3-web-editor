70fa4b7c178f4f53efce17de2b5f9dc34be6a884
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-05-05 17:18:10 -0400

    compression

--- mk_library.py
+++ mk_library.py
@@ -4,26 +4,25 @@ import os, subprocess, json
 from pathlib import Path
 
 library_zip_fn = 'dist/library.zip'
-zf = zipfile.ZipFile(library_zip_fn, 'w')
 
 os.chdir('combined_lib')
+subprocess.call(['lean', '-v'])
 subprocess.call(['leanpkg', 'build'])
 lean_p = json.loads(subprocess.check_output(['lean', '-p']))
 lean_path = [Path(p).resolve() for p in lean_p["path"]]
 
 already_seen = set()
-for p in lean_path:
-    for fn in p.glob('**/*.olean'):
-        rel = fn.relative_to(p)
-        if '_target' in rel.parts:
-            continue
-        elif rel in already_seen:
-            print('duplicate: {0}'.format(fn))
-        else:
-            content = open(fn, 'rb').read()
-            zf.writestr(zipfile.ZipInfo(filename=str(rel)), content)
-            already_seen.add(rel)
+with zipfile.ZipFile('../' + library_zip_fn, mode='w', compression=zipfile.ZIP_DEFLATED, allowZip64=False, compresslevel=9) as zf:
+    for p in lean_path:
+        print(str(p))
+        for fn in p.glob('**/*.olean'):
+            rel = fn.relative_to(p)
+            if '_target' in rel.parts:
+                continue
+            elif rel in already_seen:
+                print('duplicate: {0}'.format(fn))
+            else:
+                zf.write(fn, arcname=str(rel))
+                already_seen.add(rel)
 
 print('Created {0} with {1} olean files'.format(library_zip_fn, len(already_seen)))
-
-zf.close()

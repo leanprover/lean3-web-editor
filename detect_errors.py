85735175c48b626c4604afcee8a175ffada380d4
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-11-09 23:26:38 -0500

    more specific error detection don't want to trigger on "D	scripts/detect_errors.py'

--- detect_errors.py
+++ detect_errors.py
@@ -3,7 +3,7 @@ import sys
 
 for line in sys.stdin:
     sys.stdout.write(line)
-    if 'error' in line:
+    if 'error:' in line:
         for line in itertools.islice(sys.stdin, 20):
             sys.stdout.write(line)
         sys.exit(1)

a73f86beccb70ddee6ef498219de73da84059962
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2020-11-04 21:29:40 -0500

    try switching to github actions

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 set -e # fail on error
 
-# Make sure we have access to secure Travis environment variables.
-if ! [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
-    echo 'deploy_nightly.sh: Build is a push to master, but no secure env vars.' >&2
-    exit 1 # Something's wrong.
-fi
-
 git clone https://github.com/bryangingechen/lean-web-editor-dist.git
 cd lean-web-editor-dist
 git remote add deploy "https://$GITHUB_TOKEN@github.com/bryangingechen/lean-web-editor-dist.git"
@@ -26,7 +20,7 @@ cd ..
 # After this point, we don't use any secrets in commands.
 set -x # echo commands
 
-LATEST_BROWSER_LEAN_URL=https://github.com/leanprover-community/lean/releases/download/v$ELAN_OVERRIDE/lean-$ELAN_OVERRIDE--browser.zip
+LATEST_BROWSER_LEAN_URL=https://github.com/leanprover-community/lean/releases/download/v$LATEST_BROWSER_LEAN/lean-$LATEST_BROWSER_LEAN--browser.zip
 
 rm -f dist/*.worker.js
 npm install
@@ -44,7 +38,9 @@ cp -a dist/. lean-web-editor-dist
 cd lean-web-editor-dist
 git add -A
 git diff-index HEAD
-git diff-index --quiet HEAD || { git commit --amend --no-edit && git push deploy -f; }
+if [ "$github_repo" = "leanprover-community/lean-web-editor" ] && [ "$github_ref" = "refs/heads/master" ]; then
+    git diff-index --quiet HEAD || { git commit --amend --no-edit && git push deploy -f; }
+fi
 cd ..
 
 # push bryangingechen.github.io
@@ -52,7 +48,9 @@ cd bryangingechen.github.io
 git submodule update --init --remote
 git add -A
 git diff-index HEAD
-git diff-index --quiet HEAD || { git commit -m "lean-web-editor-dist: $(date)" && git push deploy; }
+if [ "$github_repo" = "leanprover-community/lean-web-editor" ] && [ "$github_ref" = "refs/heads/master" ]; then
+    git diff-index --quiet HEAD || { git commit -m "lean-web-editor-dist: $(date)" && git push deploy; }
+fi
 cd ..
 
 # push leanprover-community/lean-web-editor
@@ -64,4 +62,6 @@ rm -f lib*
 rm -f lean_js_*
 git add -A
 git diff-index HEAD
-git diff-index --quiet HEAD || { git commit -m "lean-web-editor: $(date)" && git push deploy; }
+if [ "$github_repo" = "leanprover-community/lean-web-editor" ] && [ "$github_ref" = "refs/heads/master" ]; then
+    git diff-index --quiet HEAD || { git commit -m "lean-web-editor: $(date)" && git push deploy; }
+fi
